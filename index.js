c9c0dbc1ec9099ad59bf7513265d5350ccda4e8f
necfol <necfoll@gmail.com>
2018-03-21 15:14:53 +0800

    增加示例

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Necfol on 3/21/18.
+ */
+import {
+  requireNativeComponent
+} from 'react-native';
+var NativeUIExample = requireNativeComponent('NativeUIExample', null);
+module.exports = NativeUIExample;
